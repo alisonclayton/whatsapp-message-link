b62b45e1e1a2ebe30fe68c7410c527081776d496
alisonclayton <alisonclayton.aac@gmail.com>
2024-02-23 11:11:52 -0300

    Treatment of Exception error handling #1 issue.

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ message = st.text_input("Message: ")
 
 message_list = message.split()
 link_out = 'Please, copy your generated [link to whatsapp with message here!](https://wa.me//55'
-link_out += wapp_number + '/?text=' + message_list[0]
+
+try:
+    link_out += wapp_number + '/?text=' + message_list[0]
+except (IndexError):
+    st.write("Please, fill in the fields correctly and press <ENTER> to get your link!")
 
 for i in range(len(message_list)):
     if i < len(message_list) - 1: link_out += '%20' + message_list[i + 1]
     elif i == len(message_list): link_out += '%20' + message_list[i]
-
-if wapp_number == '' or message == '':
-    st.write("Please, fill in the fields correctly and press <ENTER>, to get your link!")
-else:
-    link_out += ')'
-    st.markdown(link_out,unsafe_allow_html=True)
+    
